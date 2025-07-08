545526200412cde28afca6e727860bed7016dfdf
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-09 03:54:32 +0800

    Added base schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
+  out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
