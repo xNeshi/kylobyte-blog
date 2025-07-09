3945632b3de3685030e03a08146f7e4383b09aa5
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-09 11:52:13 +0800

    Added font.ts

---
+++ public/font.ts
@@ -0,0 +1,10 @@
+import { Inter, Roboto } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
