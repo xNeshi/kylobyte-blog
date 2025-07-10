6823d0d9271747e1bcacc28eabd78d779e6b6f9d
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-07-10 14:51:26 +0800

    Integrated backend to posts components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,56 @@
 import HeaderTitle from "@/components/HeaderTitle";
 import PostCard from "@/components/PostCard";
+import { fetchFeaturedPosts, fetchRecentPosts } from "@/lib/actions/posts";
 
-export default function Home() {
+export default async function Home() {
+  const recentPosts = await fetchRecentPosts();
+  const featuredPosts = await fetchFeaturedPosts();
+
+  console.log("Featured Posts:", featuredPosts);
+  console.log("Recent Posts:", recentPosts);
+
   return (
     <>
       <HeaderTitle />
 
       <section className="flex flex-col justify-center p-6 min-[1280px]:px-0">
         <h3 className="text-[22px] mb-6 font-semibold">Recent blog posts</h3>
-        <div className="grid grid-cols-1 laptop:grid-cols-2 gap-y-9 gap-x-5 w-full laptop:gap-x-6 laptop:gap-y-6">
-          {Array.from({ length: 4 }).map((_, i) => (
-            <PostCard
-              key={i}
-              postId={i}
-              recent
-              featured={i === 0 || i === 3}
-            />
-          ))}
-        </div>
+
+        {recentPosts != null && recentPosts.length > 0 ? (
+          <div className="grid grid-cols-1 laptop:grid-cols-2 gap-y-9 gap-x-5 w-full laptop:gap-x-6 laptop:gap-y-6">
+            {recentPosts.map((post, index) => (
+              <PostCard
+                key={post.id}
+                pos={index}
+                post={post}
+                recent
+                featured={index === 0 || index === 3}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500 flex items-start justify-baseline">
+            No recent posts available.
+          </p>
+        )}
       </section>
 
       <section className="flex flex-col justify-center p-6  min-[1280px]:px-0">
         <h3 className="text-[22px] mb-6 font-semibold">Featured blog posts</h3>
-        <div className="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-y-9 gap-x-5 mb-9">
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-        </div>
+        {featuredPosts != null && featuredPosts.length > 0 ? (
+          <div className="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-y-9 gap-x-5 mb-9">
+            {featuredPosts.map((post) => (
+              <PostCard
+                key={post.id}
+                post={post}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500 flex items-start justify-baseline">
+            No recent posts available.
+          </p>
+        )}
       </section>
     </>
   );
