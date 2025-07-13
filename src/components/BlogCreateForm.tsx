85a7e378546a954c91eb382bc57170f37781c09f
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-07-14 01:12:13 +0800

    Updated uploadthing and mdeditor

--- src/components/BlogCreateForm.tsx
+++ src/components/BlogCreateForm.tsx
@@ -2,12 +2,16 @@
 
 import { createBlogPost } from "@/lib/actions/posts";
 import { useUploadThing } from "@/lib/utils";
+import MDEditor from "@uiw/react-md-editor";
 import { useActionState, useState } from "react";
+import rehypeSanitize from "rehype-sanitize";
 import ImageUpload from "./ImageUpload";
 
 export const BlogCreateForm = () => {
   const [file, setFile] = useState<File[] | null>(null);
   const { startUpload } = useUploadThing("imageUploader");
+  const [value, setValue] = useState<string>("");
+
   const [state, action, isPending] = useActionState(createBlogPost, undefined);
 
   const handleSubmit = async (formData: FormData) => {
@@ -32,7 +36,6 @@ export const BlogCreateForm = () => {
       className="flex flex-col items-center max-w-[600px] justify-center w-full gap-5"
     >
       <div className="flex flex-col w-full gap-2">
-        <label htmlFor="imageUrl">Blog Cover</label>
         <ImageUpload onFileChange={setFile} />
       </div>
       <div className="flex flex-col w-full gap-2 mt-10">
@@ -41,8 +44,35 @@ export const BlogCreateForm = () => {
           type="text"
           id="title"
           name="title"
-          className="w-full border-1 rounded-full p-1 px-5"
+          className="w-full border-1 rounded-full p-1 px-5 text-[15px]"
           placeholder="Enter your blog title"
+        />
+      </div>
+
+      <div className="flex flex-col w-full gap-2">
+        <label htmlFor="content">Contents</label>
+
+        <MDEditor
+          value={value}
+          onChange={(prev) => setValue(prev ?? "")}
+          id="content"
+          preview="edit"
+          height={300}
+          className=""
+          style={{
+            borderRadius: 25,
+            overflow: "hidden",
+            border: "1px",
+            padding: "0.5rem",
+            backgroundColor: "var(--background)",
+          }}
+          textareaProps={{
+            placeholder:
+              "Describe your Project Concept and give the Necessary Details",
+          }}
+          previewOptions={{
+            rehypePlugins: [[rehypeSanitize]],
+          }}
         />
       </div>
     </form>
--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { X } from "lucide-react";
 import Image from "next/image";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export const ImageUpload = ({
   onFileChange,
@@ -31,26 +33,32 @@ export const ImageUpload = ({
   return (
     <div className="space-y-2">
       {preview ? (
-        <div className="relative w-full h-full aspect-[9/3] border-1 flex items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
+        <div className="relative w-full h-full aspect-[9/3] shadow-2xl border-0 rounded-2xl overflow-clip flex items-center justify-center">
           <Image
             src={preview}
             alt="Image preview"
             className="object-cover"
             fill
-          />
+          />{" "}
+          <Button
+            type="button"
+            onChange={(e) => {
+              e.preventDefault();
+              setPreview(null);
+              onFileChange(null);
+            }}
+            className="z-10 absolute top-2 right-2 !p-1.5 bg-gray-400 w-full h-7 tablet:w-fit text-white rounded-full hover:bg-red-600 active:bg-red-400"
+          >
+            <X />
+          </Button>
         </div>
       ) : (
-        <div className="w-full h-full aspect-[9/3] border-1 p-5 text-center flex items-center justify-center">
-          <p className="text-gray-500 text-sm">
-            No image selected. Please upload an image.
-          </p>
-        </div>
-      )}
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleFileChange}
-        className="block w-full text-sm text-gray-500
+        <div className="relative flex flex-col w-full h-full aspect-[9/3] border-1 rounded-2xl p-5 text-center items-center justify-center">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="text-sm text-gray-500
         file:mr-4 file:py-1 file:px-4
         file:rounded-full 
         file:text-sm file:font-semibold
@@ -58,7 +66,9 @@ export const ImageUpload = ({
         file:bg-background file:text-foreground
         file:hover:bg-[var(--hover-bg)] file:active:bg-[var(--active-bg)]
         file:hover:scale-95 file:transition-transform file:duration-300 file:ease-in-out"
-      />
+          />
+        </div>
+      )}
     </div>
   );
 };
