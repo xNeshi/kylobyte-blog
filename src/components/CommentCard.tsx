f1047b0d8fb6f86a00e17e2cffa9613e3a0a1486
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-15 00:01:40 +0800

    Updated ui on post and blogs

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -23,16 +23,23 @@ export const CommentCard = ({ comment }: CommentCardProps) => {
   if (!editor) return null;
 
   return (
-    <div className="flex items-start gap-3">
-      <div className="flex items-center flex-shrink-0 justify-center h-[50px] w-[50px] rounded-full bg-gray-200 dark:bg-gray-700">
+    <div className="flex items-start gap-3 tablet:gap-5">
+      <div className="flex items-center flex-shrink-0 justify-center h-[40px] w-[40px] tablet:h-[50px] tablet:w-[50px] rounded-full bg-gray-200 dark:bg-gray-700">
         A
       </div>
       <div className="flex flex-col gap-1 ">
-        <span className="inline-flex items-center gap-3">
-          <h2 className="font-bold text-[15px]">{comment.author}</h2>{" "}
-          <h3 className="opacity-80 text-[14px]">{timeAgo}</h3>
+        <span className="inline-flex items-center gap-2 tablet:gap-3">
+          <h2 className="font-bold text-[13px] tablet:text-[16px]">
+            {comment.author}
+          </h2>{" "}
+          <h3 className="opacity-80 text-[13px] tablet:text-[15px]">
+            {timeAgo}
+          </h3>
         </span>
-        <EditorContent editor={editor} />
+        <EditorContent
+          editor={editor}
+          className="text-[13px] tablet:text-[15px] "
+        />
       </div>
     </div>
   );
