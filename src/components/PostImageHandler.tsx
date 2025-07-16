b3a7e8b51168546e1399fc12ad4cf5505117a369
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-16 14:53:09 +0800

    Added loader for img

--- src/components/PostImageHandler.tsx
+++ src/components/PostImageHandler.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -10,17 +11,34 @@ type PostImageHandler = {
 
 const PostImageHandler = ({ imageUrl, title }: PostImageHandler) => {
   const [image, setImage] = useState(imageUrl);
+  const [reveal, setReveal] = useState(false);
+  const loader = reveal ? "none" : "inline-block";
+
   return (
-    <Image
-      src={image}
-      onError={() =>
-        setImage((prev) => (prev = "/images/placeholder-image.jpg"))
-      }
-      alt={title}
-      fill
-      className="object-cover"
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-    />
+    <>
+      <Image
+        src={image}
+        onError={() => {
+          setReveal(true);
+          setImage((prev) => (prev = "/images/placeholder-image.jpg"));
+        }}
+        onLoadingComplete={() => {
+          setReveal(true);
+        }}
+        alt={title}
+        fill
+        className="object-cover"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
+      <span
+        className="flex items-center justify-center bg-background w-full h-full absolute inset-0"
+        style={{
+          display: loader,
+        }}
+      >
+        <Loader2 className="w-15 h-15 animate-spin text-gray-500" />
+      </span>
+    </>
   );
 };
 
